a10b1595a4856ed71c75179d364aa0042986965b
ArguedasG <gaarguedas@estudiantec.cr>
2026-03-13 17:59:14 -0600

    Pruebas de conexión BD
    Se levantaron los contenedores y se conectó la BD con la API de NodeJS correctamente.

--- Tarea_1/backend/src/database/data-source.ts
+++ Tarea_1/backend/src/database/data-source.ts
@@ -10,7 +10,7 @@ export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || "5432"),
-  username: process.env.DB_USERNAME || process.env.DB_USER,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: true,
